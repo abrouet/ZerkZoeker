3b6066891a8e96dcd690e6d22acea49eed9f4b8e
bastiaan.andriessen <bastiaan.andriessen@flowpilots.com>
2014-07-01 15:27:15 +0200

    grave detail view + fonts loaded

--- iScrollr5/iscroll-master/js/main.js
+++ iScrollr5/iscroll-master/js/main.js
@@ -4,6 +4,10 @@
 
 document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 
+//add event handlers
+bindEvents();
+clickedGravePoint();
+
 $("#filter").keyup(function(){
 
     // Retrieve the input field text and reset the count to zero
@@ -27,3 +31,38 @@ $("#filter").keyup(function(){
     }
 
 });
+
+function clickedGravePoint(e){
+  if(e){e.preventDefault();}
+
+  //load template
+  var url = "templates/grave_detail.html";
+  $.ajax({
+      type:'GET',
+      url:url,
+      success: function(graveDetail){
+        //fill in grave point data in template
+        $('#wrapper').after(graveDetail);
+        bindEvents();
+        var originalTopMargin = $('#grave_detail').css('top');
+        $('#grave_detail').css('top', '100%').animate({
+            top: originalTopMargin
+        }, 600);
+      },
+      error: function (xhr, ajaxOptions, thrownError) {}
+  });
+}
+
+function closeGraveDetail(e){
+  console.log
+  if(e){e.preventDefault();}
+  $('#grave_detail').animate({
+      top: $(window).height()+'px'
+  }, 600, function(e){
+    $(this).remove();
+  });
+}
+
+function bindEvents(){
+  $('#close_detail').on('click', closeGraveDetail);
+}
