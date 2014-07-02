462c30f3ee17b75cb9ab9339ce1ea5f665426ac2
bastiaan.andriessen <bastiaan.andriessen@flowpilots.com>
2014-07-02 10:30:01 +0200

    cleanup + js OOP

---
+++ iScrollr5/iscroll-master/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ruben on 1-7-14.
+ */
+
+var map;
+
+(function()
+{
+    function init()
+    {
+      console.log('[main.js] init');
+      map = new Map();
+    }
+
+    init();
+
+})();
