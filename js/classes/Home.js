2e632f44338ad8cd49f2e47260a821ad9100cd7a
bastiaan.andriessen <bastiaan.andriessen@flowpilots.com>
2014-07-03 17:43:41 +0200

    edit scrollbar home

--- js/classes/Home.js
+++ js/classes/Home.js
@@ -1,8 +1,12 @@
 var Home = (function()
 {
+    var resultsScroller, arrNames, personTemplate, numResultsToShow, moreResultsAvailable;
+
     function Home()
     {
       console.log('[Home.js] init');
+      numResultsToShow = 30;
+      moreResultsAvailable = false;
 
       //load template
       $("#view1").html('').css('left',0).load("templates/home.html", function(){
@@ -29,10 +33,7 @@ var Home = (function()
                   }
               }
               $('#view_home #wrapper').css('height', $(window).height()-$('#form').offset().top-$('form').height());
-              $('#view_home #scroller, #view_home #results').css('height',
-              ((parseFloat($('#results .location:last-child').css('height'))+parseFloat($('.location').css('margin-top')))
-              *cemeteries.length)+'px');
-              var resultsScroller = new IScroll('#view_home #wrapper');
+              makeScroll();
               bindEvents();
             });
           },error: function (xhr, ajaxOptions, thrownError){}
@@ -55,9 +56,7 @@ var Home = (function()
 
     function filterKeyUp(e){
       var search = $(this).val();
-      console.log(search.length);
       if(search.length == 1){
-        console.log('test');
         updateCityList(search);
         //get all names starting with first letter entered
         var url = 'backend/index.php/getPersonByName/'+search;//by name or year
@@ -66,37 +65,28 @@ var Home = (function()
           type:'GET',
           url:url,
           success: function(names){
-            //var name = names[0]['firstName'].toLowerCase()+' '+names[0]['familyName'].toLowerCase();
-            /*$.get("templates/location.html", function(data){
-              for(var i = 0; i < cemeteries.length; i++){
-                  var html = data.replace('place', cemeteries[i]);
-                  if(cemeteries[i].length > 0){
-                    $('#results').append(html);
-                    $('#results .location:last-child').on('click', tappedLocation).on('touchend', tappedLocation).css('opacity', 0).delay(i*25).animate({
-                      opacity: 1
-                    }, 350);
-                  }
-                  if(i == cemeteries.length-1){
-                      $('#results .location:last-child').addClass('border_radius_bottom');
-                  }
-              }
-              $('#view_home #wrapper').css('height', $(window).height()-$('#form').offset().top-$('form').height());
-              $('#view_home #scroller, #view_home #results').css('height',
-              ((parseFloat($('#results .location:last-child').css('height'))+parseFloat($('.location').css('margin-top')))
-              *cemeteries.length)+'px');
-              var resultsScroller = new IScroll('#view_home #wrapper');
-              bindEvents();
-            });*/
-
+            arrNames = names;
+            $.get("templates/person_listitem.html", function(data){
+              personTemplate = data;
+              /*if(names.length < numResultsToShow){
+                numResultsToShow = names.length;
+              }*/
+              updateNameList(search);
+              makeScroll();
+            });
           },error: function (xhr, ajaxOptions, thrownError){}
         });
       }else if(search.length > 0){
         //update cities
         updateCityList(search);
+        updateNameList(search);
+        makeScroll();
       }else{
         $("#view_home .location").each(function(index, value) {
           $(this).html($(this).html().replace('<span>','').replace('</span>','')).show();
         });
+        $("#view_home .person").remove();
+        makeScroll();
       }
     }
 
@@ -110,6 +100,76 @@ var Home = (function()
             $(this).html(html).show().addClass("border_radius_bottom");
           }
       });
+    }
+
+    function updateNameList(search){
+      $("#view_home .person").remove();
+      $("#view_home .show_more_results").remove();
+      moreResultsAvailable = false;
+      numResultsToShow = 0;
+      for(var j = 0; j < arrNames.length; j++){
+        var firstName = '';
+        if(arrNames[j]['firstName']){
+          firstName = arrNames[j]['firstName'].toLowerCase();
+        }
+        var lastName = '';
+        if(arrNames[j]['familyName']){
+          lastName = arrNames[j]['familyName'].toLowerCase();
+        }
+        var birthYear = '';
+        if(arrNames[j]['birthDate']){
+          birthYear = arrNames[j]['birthDate'].substring(0, 4);
+        }
+        var deathYear = '';
+        if(arrNames[j]['dateOfDeath']){
+          deathYear = arrNames[j]['dateOfDeath'].substring(0, 4);
+        }
+        var regExpCheckString = firstName + '' + '' + lastName + '' + birthYear + '' + deathYear;
+
+        if(numResultsToShow < 30 && (regExpCheckString.indexOf(search.toLowerCase()) >= 0)){
+          var name = '';
+          if(arrNames[j]['firstName'] && arrNames[j]['familyName']){
+            name = arrNames[j]['firstName'].toLowerCase()+' '+arrNames[j]['familyName'].toLowerCase();
+          }else if(arrNames[j]['firstName']){
+            name = arrNames[j]['firstName'].toLowerCase();
+          }else if(arrNames[j]['familyName']){
+            name = arrNames[j]['familyName'].toLowerCase();
+          }
+          if(name.length > 0){
+            var html = personTemplate.replace('name', name);
+            $('#results').prepend(html);
+          }
+          numResultsToShow++;
+        }
+        if(numResultsToShow == 30){
+          moreResultsAvailable = true;
+        }
+      }
+      $("#view_home .person").each(function(index, value) {
+          if ($(this).text().search(new RegExp(search, "i")) >= 0) {
+            var html = addSpanToFilterResult($(this), search);
+            $("#view_home .person").removeClass('border_radius_bottom');
+            $(this).html(html).addClass("border_radius_bottom");
+          }
+      });
+      /*if(moreResultsAvailable){
+        var html = personTemplate.replace('name', 'show more results').replace('person','show_more_results');
+        $('#results').append(html);
+      }*/
+    }
+
+    function makeScroll(){
+      var visibleElements = 0;
+      $('#results li').each(function(index, value) {
+          if($(this).is(":visible")){
+            visibleElements++;
+          }
+      });
+      console.log('visible elements: '+visibleElements);
+      $('#view_home #scroller, #view_home #results').css('height',
+      ((parseFloat($('#results .location:last-child').css('height'))+parseFloat($('.location').css('margin-top')))
+      *visibleElements+20)+'px');
+      resultsScroller = new IScroll('#view_home #wrapper');
     }
 
     function bindEvents(){
