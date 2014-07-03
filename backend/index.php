78ecd4a0115158cff77c606c44e374fc99dedc65
bastiaan.andriessen <bastiaan.andriessen@flowpilots.com>
2014-07-03 12:51:20 +0200

    fix

--- backend/index.php
+++ backend/index.php
@@ -3,7 +3,7 @@
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
-require_once __DIR__.'/../../../vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true; //debug output
