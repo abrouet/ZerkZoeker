95524fc2121a36ac59d27afd9be5c25e5b3cea66
andreasDL <andreasdelille@hotmail.com>
2014-07-05 21:20:29 +0200

    login template

--- backend/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//settings
-global $locd; $locd = "localhost"; //location of database
-global $user; $user = "user"; //username
-global $pass; $pass = ""; //password
-global $dbna; $dbna = "LEIEDAL"; //database name
